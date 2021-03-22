4b8511ea969b5b8d2eafda573d370cfec25c2b94
Logistikon97 <64857902+Logistikon97@users.noreply.github.com>
2021-03-21 22:34:53 -0500

    usuario

--- scripts/conexion.php
+++ scripts/conexion.php
@@ -1,6 +1,6 @@
 <?php
 $database ="u125286094_turismo_yopal";
-$user = "u125286094_jcLesmes";
+$user = "u125286094_logistikon";
 $password = "tur1sm0Y0p4l";
 try{
     $con= new PDO('mysql:host=localhost;dbname='.$database,$user,$password);
